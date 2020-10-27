7160d7e26adee08ffab0bb28e94cc24165107a06
Mike O'Brien <mikeob9@gmail.com>
2020-10-27 19:04:06 -0400

    Automatically unzip detection files.

--- R/downloading.R
+++ R/downloading.R
@@ -1,19 +1,24 @@
 #'
 #' @export
 
-get_project_file <- function(file = NULL, project_number = NULL, project = NULL,
+get_file <- function(file = NULL, project_number = NULL, project = NULL,
                              index = NULL, url = NULL, ...){
   if(!is.null(url)){
     GET_header <- httr::GET(url)
 
     response <-  httr::GET(
       url,
-      httr::write_disk(...,
-                       sub('.*filename=', '',
-                           httr::headers(GET_header)$'content-disposition'))
+      httr::write_disk(..., sub('.*filename=', '',
+                                httr::headers(GET_header)$'content-disposition'))
     )
 
     cat('File saved to', file.path(response$content))
+
+    if(grepl('zip', response$content)){
+      unzip(file.path(response$content))
+
+      cat('\nFile unzipped to', unzip(file.path(response$content), list = T)$Name)
+    }
 
   } else{
 
@@ -24,3 +29,4 @@ get_project_file <- function(file = NULL, project_number = NULL, project = NULL,
     }
   }
 }
+
