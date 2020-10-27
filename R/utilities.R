ca0b1c09c058b92f3f09e4689b95233f24d8b232
Mike O'Brien <mikeob9@gmail.com>
2020-10-27 18:54:26 -0400

    Move matos_login to utilities.

--- R/utilities.R
+++ R/utilities.R
@@ -35,3 +35,25 @@ html_table_to_df <- function(html_file_list){
 
   df[, !names(df) %in% 'Download']
 }
+
+#'
+#' @export
+matos_login <- function(){
+  login_response <- httr::POST(
+    'https://matos.asascience.com/account/login',
+    body = list(
+      UserName = rstudioapi::showPrompt(
+        title = "Username", message = "Please enter username.", default = ""),
+      Password = rstudioapi::askForPassword('Please enter password.')
+    )
+  )
+
+  if(grepl('login', login_response)){
+    rstudioapi::showDialog('Login unsuccessful :(',
+                           'Your username/password combination was not recognized. Re-run matos_login before continuing.')
+  } else{
+    rstudioapi::showDialog('Login successful!',
+                           'You are now logged into your MATOS profile.')
+  }
+
+}
