0673283d180c43fd5990783589a94950ffe9836b
Mike O'Brien <mikeob9@gmail.com>
2020-10-27 17:02:40 -0400

    Start documentation.

---
+++ R/matos-package.R
@@ -0,0 +1,12 @@
+#' @name matos
+#' @docType package
+#'
+#' @importFrom magrittr "%>%"
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
