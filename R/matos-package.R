abacfb1452696b8174bb27f74a3c5d8f4db7818b
Mike O'Brien <mikeob9@gmail.com>
2020-10-28 13:43:44 -0400

    Move pipe operator doc.

--- R/matos-package.R
+++ R/matos-package.R
@@ -6,6 +6,18 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
