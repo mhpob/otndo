5c8b80322cc1942fc49d0be54a002fe48a86db8b
Mike O'Brien <mikeob9@gmail.com>
2020-10-27 20:48:52 -0400

    Document listing files functions.

--- R/listing.R
+++ R/listing.R
@@ -1,5 +1,37 @@
+#' List MATOS project files
 #'
+#' These functions list the file names, types, upload date, and URLs of MATOS
+#' project files -- basically everything you see in the *Data Extraction Files*
+#' or *Project Files* sections of your project page. Because it is from your
+#' project page, you **need to** first log in using \code{\link{matos_login}}.
+#'
+#' @section Details:
+#' \code{detection_files} and \code{project_files} are wrappers around a
+#' web-scraping routine: 1) find the project number if not provided, 2) download
+#' the HTML table, 3) parse the URL for each file, and 4) combine the table and
+#' URLs into a data frame. These functions are most useful when investigating what
+#' files you have available, and then downloading them with \code{\link{get_file}}.
+#'
+#' \code{detection_files} lists files associated with the ACT_MATOS OTN node. These
+#' are files listed on the *Data Extraction Files* page.
+#'
+#' \code{project_files} lists tag and receiver metadata files that have been
+#' uploaded by the user. These are the files listed on the *Project Files* page.
+#'
+#' @param project Either the project number (the number in your project page URL)
+#'      or the full name of the project (the big name in bold on your project page,
+#'      *not* the "Project Title")
+#' @return A data frame with columns of "File Name", "File Type", "Upload Date", and "url".
+#'
+#' @name list_files
 #' @export
+#' @examples
+#' # Select using project number
+#' detection_files(87)
+#' project_files(87)
+#'
+#' # Select using project name
+#' detection_files('umces boem offshore wind energy')
 
 detection_files <- function(project_number = NULL, project = NULL){
   if(is.null(project_number)){
@@ -20,7 +52,8 @@ detection_files <- function(project_number = NULL, project = NULL){
 }
 
 
-#'
+
+#' @rdname list_files
 #' @export
 
 project_files <- function(project_number = NULL, project = NULL){
