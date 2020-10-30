a9eeb2392c82ca92f2b3760212e4aa1c4260f65a
Mike O'Brien <mikeob9@gmail.com>
2020-10-30 12:07:42 -0400

    Add documentation, change from file_type arg to data_type

--- R/uploading.R
+++ R/uploading.R
@@ -1,9 +1,39 @@
+#' Upload files to MATOS
 #'
+#' This function lets you post files to your projects. You will need to log in
+#' before uploading.
+#'
+#' \code{post_file} takes a project name or number, the file you want to upload,
+#' and the type of file you want to upload (defaulting to VRL transmitter detections).
+#'
+#' @param project The name (character) or number (numeric) of the project to which
+#'     you wish post your file.
+#' @param file The file you wish to upload. If the file is located in your working
+#'     directory, this can be just the filename and extension. You will need to
+#'     provide the full file location if it is located elsewhere.
+#' @param data_type Character string. The data type that you are uploading. One of:
+#'     "new_tags" (default), "receivers", "vrl_detections", "csv_detections",
+#'     or "events".
+#'
+#' @details
+#'     If data_type is "new_tags" or "receivers", CSV and XLS/XLSX files are accepted;
+#'     if "detections", only VRL and CSV files are accepted; if "events", only CSV is
+#'     accepted.
 #'
 #' @export
+#' @examples
+#' \dontrun{
+#' # Tag detections, the default
+#' post_file(87, 'your_VRL_file.vrl')
+#' post_file('umces boem offshore wind energy', 'c:/wherever/your_CSV_detections.csv')
+#'
+#' # Receiver metadata
+#' post_file('umces boem offshore wind energy', 'your_receiver_metadata.xlsx', 'receivers')
+#' }
+
 post_file <- function(project, file,
-                      file_type = c('receivers', 'new_tags', 'events',
-                                    'csv_detections', 'vrl_detections')){
+                      data_type = c('new_tags', 'receivers', 'vrl_detections',
+                                    'csv_detections', 'events')){
 
   # Check that file exists
   file <- normalizePath(file, mustWork = F)
@@ -14,16 +44,16 @@ post_file <- function(project, file,
 
   }
 
-  # Check and repair file_type argument
-  file_type <- match.arg(file_type)
+  # Check and repair data_type argument
+  data_type <- match.arg(data_type)
 
   # Check that file extensions match the expected file type.
-  if(file_type %in% c('receivers', 'new_tags') &&
+  if(data_type %in% c('receivers', 'new_tags') &&
      !grepl('xls|csv', tools::file_ext(file), ignore.case = T)){
 
     stop('File is not saved as the correct type: should be CSV, XLS, or XLSX.')
 
-  } else if(file_type %in% c('events', 'csv_detections') &&
+  } else if(data_type %in% c('events', 'csv_detections') &&
             tolower(tools::file_ext(file)) != 'csv'){
 
     stop('File is not saved as the correct type: should be CSV.')
@@ -35,8 +65,8 @@ post_file <- function(project, file,
 
   }
 
-  # Convert file_type to the expected input numbers
-  file_num <- switch(file_type,
+  # Convert data_type to the expected input numbers
+  data_num <- switch(data_type,
     new_tags = 1,
     receivers = 2,
     csv_detections = 3,
@@ -66,7 +96,7 @@ post_file <- function(project, file,
     'https://matos.asascience.com/report/uploadReport',
     body = list(
       pid = project,
-      df = file_num,
+      df = data_num,
       file = httr::upload_file(file)
     ),
     encode = 'multipart'
