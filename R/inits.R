baf12b50842f7f91db2a6068911abf99a79ce2bb
Mike O'Brien <mikeob9@gmail.com>
2020-11-02 12:22:27 -0500

    Notify about user agreement on attach.

---
+++ R/inits.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(paste(
+  "By continuing, you are agreeing to the ACT Network MATOS User Agreement and Data Policy, Version 1.1:",
+  "https://matos.asascience.com/static/MATOS.User.Agreement.V1.1.pdf", sep = '\n\n'))
+}
