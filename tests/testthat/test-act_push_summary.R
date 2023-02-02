91046bf71384ff8b3244cf5574c53e522747c19e
Mike O'Brien <mikeob9@gmail.com>
2023-02-02 16:48:08 -0500

    act push test (kind of)

--- tests/testthat/test-act_push_summary.R
+++ tests/testthat/test-act_push_summary.R
@@ -2,4 +2,4 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-matos_project <- 192
+# act_push_summary(192, qualified = list.files(pattern = '_qualified.*csv', full.names = T), unqualified = list.files(pattern = '_unqualified.*csv',full.names = T), deployment = 'c:/users/darpa2/analysis/kennebec-sturgeon-monitoring/act-matos/master_metadata_deployment.xlsx')
