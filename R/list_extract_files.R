7f51ff1d6059e837cae7a530becb445be421d9cb
Mike O'Brien <mikeob9@gmail.com>
2023-02-02 20:48:33 -0500

    split list_files

--- R/list_extract_files.R
+++ R/list_extract_files.R
@@ -1,54 +1,46 @@
-#' List MATOS project files
+#' List OTN data extraction files
 #'
-#' These functions list the file names, types, upload date, and URLs of MATOS
-#' project files -- basically everything you see in the *Data Extraction Files*
-#' or *Project Files* sections of your project page. Because it is from your
-#' project page, you **need to** first log in using \code{\link{matos_login}}.
+#' This function list the file names, types, upload date, and URLs of OTN data
+#' extraction files -- basically everything you see in the *Data Extraction Files*
+#' section of your project page. Because it is from your project page, you will be
+#' prompted to log in.
 #'
 #' @section Details:
-#' \code{detection_files} and \code{project_files} are wrappers around a
-#' web-scraping routine: 1) find the project number if not provided, 2) download
+#' \code{list_extract_files} is a wrapper around a web-scraping routine:
+#' 1) find the project number if not provided, 2) download
 #' the HTML table, 3) parse the URL for each file, and 4) combine the table and
-#' URLs into a data frame. These functions are most useful when investigating what
+#' URLs into a data frame. This function is most useful when investigating what
 #' files you have available, and then downloading them with \code{\link{get_file}}.
 #'
-#' \code{detection_files} lists files associated with the ACT_MATOS OTN node. These
+#' \code{list_extract_files} lists files associated with the ACT_MATOS OTN node. These
 #' are files listed on the *Data Extraction Files* page.
-#'
-#' \code{project_files} lists tag and receiver metadata files that have been
-#' uploaded by the user. These are the files listed on the *Project Files* page.
 #'
 #' @param project Either the project number (the number in your project page URL)
 #'     or the full name of the project (the big name in bold on your project page,
 #'     *not* the "Project Title").
-#' @param data_type one of "extraction" (default), or "project", which will list
-#'     the data extraction or project files, respectively. Partial matching
-#'     is allowed, and will repair to the correct argument if spaces or the words
-#'     "data"/"file(s)" are included.
 #' @param detection_type one of NULL (default), "matched", "qualified", "sentinel_tag",
-#'     or "unqualified"; used in a call to \code{list_files} under the hood. If NULL,
-#'     all detection types will be listed. Partial matching is allowed, and will repair
+#'     or "unqualified". Partial matching is allowed, and will repair
 #'     to the correct argument if spaces or the words "detection(s)" are included.
 #'     More information on data types can be found on \href{https://members.oceantrack.org/data/otn-detection-extract-documentation-matched-to-animals}{OTN's website}.
 #' @param since Only list files uploaded after this date. Optional, but must be
 #'      in YYYY-MM-DD format.
 #'
-#' @return A data frame with columns of "File.Name", "File.Type", "Upload.Date", and "url".
+#' @return A data frame with columns of "project", "file_type", "detection_type", 'detection_year', 'upload_date', 'file_name', and "url".
 #'
 #' @export
 #' @examples
 #' \dontrun{
-#' # Select using project number, this defaults to grabbing the data extraction files
-#' list_files(87)
+#' # List all extraction files using project number
+#' list_extract_files(87)
 #'
-#' # Or, grab the project files
-#' list_files(project = 87, data_type = 'project')
+#' # Or, just grab the matched receiver detections
+#' list_files(project = 87, detection_type = 'qualified')
 #'
-#' # Select using project name
+#' # OR list files using the project name
 #' list_files('umces boem offshore wind energy')
 #' }
 
-list_files <- function(project = NULL, data_type = c('extraction', 'project'),
+list_extract_files <- function(project = NULL,
                        detection_type = c('all', 'matched', 'qualified',
                                           'sentinel_tag', 'unqualified'),
                        since = NULL){
@@ -69,7 +61,7 @@ list_files <- function(project = NULL, data_type = c('extraction', 'project'),
 
   # Scrape table and list files
   # This calls login_check() under the hood
-  files_html <- get_file_list(project, data_type = data_type)
+  files_html <- get_file_list(project, data_type = 'dataextractionfiles')
 
   files <- html_table_to_df(files_html)
 
