e3f3a3c40a89142b800843da66861d01738d43a8
Mike O'Brien <mikeob9@gmail.com>
2023-02-11 18:06:58 -0500

    try to get rcmd to pass me

--- R/matos-package.R
+++ R/matos-package.R
@@ -9,6 +9,19 @@
 #' @importFrom utils download.file
 #' @importFrom utils txtProgressBar
 #' @importFrom utils setTxtProgressBar
+#' @importFrom data.table data.table
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#' @importFrom data.table .BY
+#' @importFrom data.table .N
+#' @importFrom data.table .I
+#' @importFrom data.table .GRP
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .EACHI
+#' @importFrom ggplot2 ggplot
+#' @importFrom gt gt
+#' @importFrom mapview mapview
+#' @importFrom sf read_sf
 #' @keywords internal
 "_PACKAGE"
 
