91c423232549ed2249db95460f417953f064991d
Mike O'Brien <mikeob9@gmail.com>
2023-02-11 13:07:50 -0500

    rename test placeholder

--- tests/testthat/test-act_push_summary.R
+++
@@ -1,5 +0,0 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-# act_push_summary(192, qualified = list.files(pattern = '_qualified.*csv', full.names = T), unqualified = list.files(pattern = '_unqualified.*csv',full.names = T), deployment = 'c:/users/darpa2/analysis/kennebec-sturgeon-monitoring/act-matos/master_metadata_deployment.xlsx')
