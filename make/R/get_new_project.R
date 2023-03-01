2a2e0e4555f2621ef98ed7d8f1507a423bb8b080
Mike O'Brien <mikeob9@gmail.com>
2023-03-01 18:02:35 -0500

    get correct files and move into repo

--- make/R/get_new_project.R
+++
@@ -1,5 +0,0 @@
-get_new_project <- function(){
-  login_check()
-
-  browseURL('https://matos.asascience.com/project/add')
-}
--- make/R/inits.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-  cli::cli_text(
-    "By continuing, you are agreeing to the ACT Network MATOS User Agreement and Data Policy, Version 1.2:\n\n",
-  "{.url https://matos.asascience.com/static/MATOS.User.Agreement.V1.1.pdf}")
-  )
-}
