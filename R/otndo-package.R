c936fee4774d72f453c3ff4ef91972211076e2fe
Mike O'Brien <mikeob9@gmail.com>
2023-03-01 16:46:06 -0500

    initial imports

---
+++ R/otndo-package.R
@@ -0,0 +1,16 @@
+#' @name otndo
+#' @docType package
+#'
+#' @importFrom utils unzip
+#' @importFrom utils URLencode
+#' @importFrom data.table data.table
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#' @importFrom data.table .BY
+#' @importFrom data.table .N
+#' @importFrom data.table .I
+#' @importFrom data.table .GRP
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .EACHI
+#' @keywords internal
+"_PACKAGE"
