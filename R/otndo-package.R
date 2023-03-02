ff3b7bec47583658adcf3efcda77ea5716d6dafa
Mike O'Brien <mikeob9@gmail.com>
2023-03-02 08:09:46 -0500

    update imports

--- R/otndo-package.R
+++ R/otndo-package.R
@@ -1,16 +1,17 @@
-#' @name otndo
-#' @docType package
-#'
-#' @importFrom utils unzip
-#' @importFrom utils URLencode
-#' @importFrom data.table data.table
-#' @importFrom data.table :=
-#' @importFrom data.table .SD
-#' @importFrom data.table .BY
-#' @importFrom data.table .N
-#' @importFrom data.table .I
-#' @importFrom data.table .GRP
-#' @importFrom data.table .NGRP
-#' @importFrom data.table .EACHI
 #' @keywords internal
 "_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table :=
+#' @importFrom data.table data.table
+#' @importFrom utils unzip
+#' @importFrom utils URLencode
+## usethis namespace: end
+NULL
