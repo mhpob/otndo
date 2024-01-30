b54d33065eabd23cae7bf7bcfb674d589d46743f
Mike O'Brien <mikeob9@gmail.com>
2024-01-30 17:57:02 -0500

    create pi_summary

---
+++ R/pi_summary.R
@@ -0,0 +1,89 @@
+pi_summary <- function(matched, type = c('receiver', 'tag')){
+  if(type == 'tag'){
+    pis <- unique(matched, by = c("detectedby", "contact_poc", "contact_pi"))
+    pis[, ":="(
+      PI = strsplit(contact_pi, " \\(|\\)(, )?"),
+      POC = strsplit(contact_poc, " \\(|\\)(, )?")
+    )]
+  } else {
+    pis <- unique(qualified, by = c("trackercode"))
+    pis[, ":="(
+      PI = strsplit(tag_contact_pi, " \\(|\\)(, )?"),
+      POC = strsplit(tag_contact_poc, " \\(|\\)(, )?")
+    )]
+  }
+
+
+  pis[, ":="(
+    PI = lapply(X = PI, function(.) .[!grepl("@", .)]),
+    POC = lapply(X = POC, function(.) .[!grepl("@", .)]),
+    PI_emails = lapply(X = PI, function(.) .[grepl("@", .)]),
+    POC_emails = lapply(X = POC, function(.) .[grepl("@", .)])
+  )]
+
+  pis[, ":="(PI = unlist(
+    lapply(
+      X = PI,
+      function(.) paste(., collapse = ", ")
+    )
+  ),
+  POC = unlist(
+    lapply(
+      X = POC,
+      function(.) paste(., collapse = ", ")
+    )
+  ),
+  emails = t(
+    mapply(
+      c,
+      PI_emails, POC_emails,
+      # Need simplify=F arg to mapply in case no rows have
+      # multiple emails in a column: simplify=T
+      # will "simplify" to a matrix rather than keep as a list
+      SIMPLIFY = FALSE
+    )
+  ))] |>
+    suppressWarnings()
+
+  pis[, ":="(emails = unlist(
+    lapply(
+      lapply(
+        emails,
+        unique
+      ),
+      paste,
+      collapse = "\n"
+    )
+  ),
+  PI_emails = unlist(
+    lapply(
+      lapply(
+        PI_emails,
+        unique
+      ),
+      paste,
+      collapse = ","
+    )
+  ),
+  POC_emails = unlist(
+    lapply(
+      lapply(
+        POC_emails,
+        unique
+      ),
+      paste,
+      collapse = ","
+    )
+  ))]
+
+  if(type == 'tag'){
+    pis <- pis[, .(detectedby, PI, POC, PI_emails, POC_emails)]
+    setnames(pis, 'detectedby', 'project_name')
+  } else {
+    pis <- pis[, .(trackercode, PI, POC, PI_emails, POC_emails)]
+    setnames(pis, 'trackercode', 'project_name')
+  }
+
+
+  pis[]
+}
---
+++ tests/testthat/test-tag_pi_summary.R
@@ -0,0 +1,80 @@
+
+td <- file.path(tempdir(), "matos_test_files")
+dir.create(td)
+
+download.file(
+  paste0(
+    "https://members.oceantrack.org/data/repository/",
+    "pbsm/detection-extracts/pbsm_matched_detections_2018.zip"
+  ),
+  destfile = file.path(td, "pbsm_matched_detections_2018.zip")
+)
+unzip(file.path(td, "pbsm_matched_detections_2018.zip"),
+      exdir = td
+)
+
+matched <- file.path(
+  td,
+  "pbsm_matched_detections_2018.csv"
+)
+
+matched <- write_to_tempdir(
+  type = "matched",
+  files = matched,
+  temp_dir = td
+)
+library(data.table)
+
+matched <- fread(matched)
+matched[, day := as.Date(datecollected)]
+
+test_that("returns correct class", {
+  expect_s3_class(
+    pi_table <- tag_pi_summary(matched),
+    c('data.table', 'data.frame'),
+    exact = TRUE
+  )
+
+  expect_type(
+    pi_table$project_name,
+    'character'
+  )
+  expect_type(
+    pi_table$PI,
+    'character'
+  )
+  expect_type(
+    pi_table$POC,
+    'character'
+  )
+  expect_type(
+    pi_table$PI_emails,
+    'character'
+  )
+  expect_type(
+    pi_table$POC_emails,
+    'character'
+  )
+})
+
+test_that("right things are returned", {
+  pi_table <- tag_pi_summary(matched)
+
+  expect_equal(
+    ncol(pi_table),
+    5
+  )
+
+  expect_named(
+    pi_table,
+    c("project_name", "PI", "POC", "PI_emails", "POC_emails" )
+  )
+
+  # contains emails
+  expect_true(
+    all(grepl('@', pi_table$PI_emails))
+  )
+  expect_true(
+    all(grepl('@', pi_table$POC_emails))
+  )
+})
