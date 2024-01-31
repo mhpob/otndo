cc6e242f0f5983b5f7f804784222b86313bdc63e
Mike O'Brien <mikeob9@gmail.com>
2024-01-31 08:28:28 -0500

    create prep_station_table

---
+++ R/prep_station_table.R
@@ -0,0 +1,107 @@
+#' Prepare the station summary table
+#'
+#' @param matched OTN detections. "Matched" detections for tag data and "qualified"
+#'  detections for receiver data
+#' @param pis PI contact table from `project_contacts`. Optional if prepping a
+#'  receiver summary.
+#' @param type type of data to be summarized.
+#'
+#' @examples
+#' # Set up example data
+#' td <- file.path(tempdir(), "otndo_example")
+#' dir.create(td)
+#'
+#' # For tag data
+#' download.file(
+#'   paste0(
+#'     "https://members.oceantrack.org/data/repository/",
+#'     "pbsm/detection-extracts/pbsm_matched_detections_2018.zip"
+#'   ),
+#'   destfile = file.path(td, "pbsm_matched_detections_2018.zip")
+#' )
+#' unzip(file.path(td, "pbsm_matched_detections_2018.zip"),
+#'   exdir = td
+#' )
+#'
+#' matched <- read.csv(file.path(
+#'   td,
+#'   "pbsm_matched_detections_2018.csv"
+#' ))
+#'
+#' pis <- project_contacts(matched, type = "tag")
+#'
+#' # Actually run the function
+#' prep_station_table(matched, type = "tag", pis)
+#'
+#'
+#'
+#' # For receiver data
+#' download.file("https://members.oceantrack.org/data/repository/pbsm/detection-extracts/pbsm_qualified_detections_2018.zip",
+#' destfile = file.path(td, "pbsm_qualified_detections_2018.zip"))
+#'
+#' unzip(file.path(td, "pbsm_qualified_detections_2018.zip"), exdir = td)
+#'
+#' qualified <- read.csv(file.path(td, "pbsm_qualified_detections_2018.csv"))
+#'
+#' # Actually run the function
+#' prep_station_table(qualified, type = 'receiver')
+#'
+#' # Clean up
+#' unlink(td, recursive = TRUE)
+#'
+#'
+#' @returns For tag data, a data.table with the PI, project, station, number of
+#'  detections, and number of individuals heard. For receiver data, a data.table
+#'  with the station, number of detections, and number of individuals heard
+#'  (assuming that the PI and POC is you).
+prep_station_table <- function(matched, type = c('tag', 'receiver'),
+                               pis = NULL){
+  matched <- data.table::data.table(matched)
+
+  if(type == 'tag'){
+    station_summary <- merge(
+      matched[, .(detections = .N), by = c("station", "detectedby")],
+      unique(matched, by = c("tagname", "station"))[, .(
+        individuals = .N,
+        long = mean(longitude),
+        lat = mean(latitude)
+      ),
+      by = "station"
+      ]
+    )
+
+    setnames(station_summary, 'detectedby', 'project_name')
+
+    station_summary <- merge(
+      station_summary,
+      pis[, .(project_name, PI)],
+      by = "project_name"
+    )
+
+  }else {
+    station_summary <- merge(
+      matched[, .(detections = .N), by = "station"],
+      unique(matched, by = c("fieldnumber", "station"))[, .(
+        individuals = .N,
+        long = mean(longitude),
+        lat = mean(latitude)
+      ),
+      by = "station"
+      ]
+    )
+  }
+
+  setorder(station_summary, -lat, long)
+
+  if(type == 'tag'){
+    station_summary <- station_summary[, .(PI, project_name, station,
+                                           detections, individuals)]
+    setnames(station_summary, c("PI", "Project", "Station",
+                                "Detections", "Individuals"))
+  } else {
+    station_summary <- station_summary[, .(station, detections, individuals)]
+    setnames(station_summary, c("Station", "Detections", "Individuals"))
+  }
+
+  station_summary[]
+}
