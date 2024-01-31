476a7059ec12060fa457b64574f241948b25eaa3
Mike O'Brien <mikeob9@gmail.com>
2024-01-31 08:42:29 -0500

    create prep_station_spatial

---
+++ R/prep_station_spatial.R
@@ -0,0 +1,42 @@
+#'
+#'
+prep_station_spatial <- function(matched, type = c('tag', 'receiver')){
+  matched <- data.table::data.table(matched)
+
+  if(type == 'tag'){
+    station_spatial <- unique(matched, by = c("station", "detectedby"))
+
+    station_spatial <- station_spatial[, station := toupper(station)]
+
+  } else {
+    station_spatial <- unique(matched, by = "station")
+  }
+
+
+  station_spatial <- station_spatial[
+    station_summary[, Station := toupper(Station)], ,
+    on = c("station" = "Station")
+  ]
+
+
+  if(type == 'tag'){
+    station_spatial <- station_spatial[, .(
+      station, Detections, Individuals,
+      longitude, latitude, PI, detectedby
+    )]
+  } else {
+    station_spatial <- station_spatial[, .(
+      station, Detections, Individuals, longitude, latitude
+    )]
+  }
+
+  sf::st_as_sf(
+    station_spatial,
+    coords = c("longitude", "latitude"),
+    crs = 4326
+  )
+
+  # For use if switching to leaflet
+  # station_center <- st_centroid(st_union(station_spatial))
+  # station_center <- st_coordinates(station_center)
+}
