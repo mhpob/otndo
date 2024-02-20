455308e71cf4bc9cc20337da504ccb52fcd96913
Mike O'Brien <mikeob9@gmail.com>
2024-02-20 18:52:41 -0500

    add receiver test and test setup function

--- tests/testthat/test-project_contacts.R
+++ tests/testthat/test-project_contacts.R
@@ -1,33 +1,39 @@
 td <- file.path(tempdir(), "matos_test_files")
 dir.create(td)
-
-download.file(
-  paste0(
-    "https://members.oceantrack.org/data/repository/",
-    "pbsm/detection-extracts/pbsm_matched_detections_2018.zip"
-  ),
-  destfile = file.path(td, "pbsm_matched_detections_2018.zip")
-)
-unzip(file.path(td, "pbsm_matched_detections_2018.zip"),
-  exdir = td
-)
-
-matched <- file.path(
-  td,
-  "pbsm_matched_detections_2018.csv"
-)
-
-matched <- write_to_tempdir(
-  type = "matched",
-  files = matched,
-  temp_dir = td
-)
 library(data.table)
 
-matched <- fread(matched)
-matched[, day := as.Date(datecollected)]
+set_up_test_project_contacts <- function(type, td){
+  file_base <- paste0("pbsm_", type, "_detections_2018")
+  file_loc <- file.path(td, paste0(file_base, ".zip"))
+  download.file(
+    paste0(
+      "https://members.oceantrack.org/data/repository/",
+      "pbsm/detection-extracts/",
+      paste0(file_base, ".zip")
+    ),
+    destfile = file_loc
+  )
+  unzip(file_loc,
+        exdir = td)
 
-test_that("returns correct class", {
+  contacts <- file.path(td, paste0(file_base, ".csv"))
+
+  contacts <- write_to_tempdir(
+    type = type,
+    files = contacts,
+    temp_dir = td
+  )
+
+  contacts <- fread(contacts)
+  contacts[, day := as.Date(datecollected)]
+
+  contacts
+}
+
+matched <- set_up_test_project_contacts("matched", td)
+qualified <- set_up_test_project_contacts("qualified", td)
+
+test_that("returns correct class for tags", {
   expect_s3_class(
     pi_table <- project_contacts(matched, type = "tag"),
     c("data.table", "data.frame"),
@@ -56,7 +62,7 @@ test_that("returns correct class", {
   )
 })
 
-test_that("right things are returned", {
+test_that("right things are returned for tags", {
   pi_table <- project_contacts(matched, type = "tag")
 
   expect_equal(
@@ -77,3 +83,58 @@ test_that("right things are returned", {
     all(grepl("@", pi_table$POC_emails))
   )
 })
+
+
+test_that("returns correct class for receivers", {
+  expect_s3_class(
+    pi_table <- project_contacts(matched, type = "receivers"),
+    c("data.table", "data.frame"),
+    exact = TRUE
+  )
+
+  expect_type(
+    pi_table$project_name,
+    "character"
+  )
+  expect_type(
+    pi_table$PI,
+    "character"
+  )
+  expect_type(
+    pi_table$POC,
+    "character"
+  )
+  expect_type(
+    pi_table$PI_emails,
+    "character"
+  )
+  expect_type(
+    pi_table$POC_emails,
+    "character"
+  )
+})
+
+test_that("right things are returned for receivers", {
+  pi_table <- project_contacts(matched, type = "receivers")
+
+  expect_equal(
+    ncol(pi_table),
+    5
+  )
+
+  expect_named(
+    pi_table,
+    c("project_name", "PI", "POC", "PI_emails", "POC_emails")
+  )
+
+  # contains emails
+  expect_true(
+    all(grepl("@", pi_table$PI_emails))
+  )
+  expect_true(
+    all(grepl("@|", pi_table$POC_emails))
+  )
+})
+
+
+unlink(td, recursive = TRUE)
