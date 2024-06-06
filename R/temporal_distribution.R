2ea7c2a61cea81e241b5302fbe2594963e672852
Mike O'Brien <mikeob9@gmail.com>
2024-06-06 16:54:02 -0400

    update for unqualified detections

--- R/temporal_distribution.R
+++ R/temporal_distribution.R
@@ -1,33 +1,46 @@
 #' @export
-temporal_distribution <- function(matched, type = c("tag", "receiver")) {
-  matched <- data.table::data.table(matched)
+temporal_distribution <- function(extract, type = c("tag", "receiver")) {
+  extract <- data.table::data.table(extract)
 
-  if (type == "receiver") {
-    matched[, day := as.Date(datecollected)]
-    matched <- unique(matched, by = c("trackercode", "day"))
-    matched[, detectedby_plot := gsub(".*\\.", "", trackercode)]
+  if(length(grep("^tag", names(extract))) == 0) {
+    # run this if unqualified detections were supplied
+    subtitle <- "Temporal distribution of unmatched detections by project"
+
+    extract[, day := as.Date(datecollected)]
+    extract <- unique(extract, by = c("station", "day"))
+    extract[, detectedby_plot := station]
+
+  } else {
+
+    if (type == "receiver") {
+      extract[, day := as.Date(datecollected)]
+      extract <- unique(extract, by = c("trackercode", "day"))
+      extract[, detectedby_plot := gsub(".*\\.", "", trackercode)]
+    }
+    if (type == "tag") {
+      proj_order_ns <- extract[, list(lat = median(latitude)), by = "detectedby"]
+      data.table::setorder(proj_order_ns, lat)
+
+      extract[, detectedby_plot := factor(
+        gsub(".*\\.", "", detectedby),
+        ordered = T,
+        levels = gsub(
+          ".*\\.", "",
+          proj_order_ns$detectedby
+        )
+      )]
+
+      extract <- unique(extract, by = c("detectedby", "day"))
+    }
+
+    subtitle <- "Temporal distribution of detections by project"
+
   }
-  if (type == "tag") {
-    proj_order_ns <- matched[, list(lat = median(latitude)), by = "detectedby"]
-    data.table::setorder(proj_order_ns, lat)
-
-    matched[, detectedby_plot := factor(
-      gsub(".*\\.", "", detectedby),
-      ordered = T,
-      levels = gsub(
-        ".*\\.", "",
-        proj_order_ns$detectedby
-      )
-    )]
-
-    matched <- unique(matched, by = c("detectedby", "day"))
-  }
-
-  ggplot2::ggplot(matched) +
+  ggplot2::ggplot(extract) +
     ggplot2::geom_tile(ggplot2::aes(x = day, y = detectedby_plot)) +
     ggplot2::labs(
       x = "", y = "",
-      subtitle = "Temporal distribution of detections by project"
+      subtitle = subtitle
     ) +
     ggplot2::theme_minimal()
 }
