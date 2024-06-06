d9332ee54b089768406eeb25a2b54d7488a6f562
Mike O'Brien <mikeob9@gmail.com>
2024-06-06 13:52:46 -0400

    add match map function

---
+++ R/match_map.R
@@ -0,0 +1,25 @@
+#'
+#'
+
+match_map <- function(otn_tables){
+  natural_earth <- sf::st_read(
+    system.file("ne_110m_coastline.gpkg",
+                package = "otndo"
+    ),
+    quiet = T
+  )
+
+  otn_sf <- otn_tables$otn_resources_metadata_points |>
+    sf::st_as_sf(wkt = "the_geom", crs = 4326)
+  otn_limits <- sf::st_bbox(otn_sf)
+
+  ggplot2::ggplot() +
+    ggplot2::geom_sf(data = natural_earth) +
+    ggplot2::geom_sf(data = otn_sf, fill = NA, color = "blue") +
+    ggplot2::coord_sf(
+      xlim = c(otn_limits["xmin"] - 5, otn_limits["xmax"] + 5),
+      ylim = c(otn_limits["ymin"] - 5, otn_limits["ymax"] + 5)
+    ) +
+    ggplot2::labs(title = "Geographic extent of detected projects") +
+    ggplot2::theme_minimal()
+}
