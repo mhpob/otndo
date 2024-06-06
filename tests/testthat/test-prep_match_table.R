60b920b1c09ef0df0031ad10012001e605624897
Mike O'Brien <mikeob9@gmail.com>
2024-06-06 19:43:20 -0400

    add match_table test

--- tests/testthat/test-prep_match_table.R
+++ tests/testthat/test-prep_match_table.R
@@ -1,3 +1,39 @@
-test_that("test for prep_match_table", {
-  skip("Test TBD")
+test_that("creates a js table for receiver data", {
+  qual <- read.csv(qualified_path)
+
+  tbl <- match_table(qual, "receiver")
+
+  expect_s3_class(tbl, c("reactable", "htmlwidget"), exact = TRUE)
+  expect_type(tbl$x, "list")
+
+  # 9 columns
+  expect_length(tbl$x$tag$attribs$columns, 9)
+
+  # Correct names
+  expect_equal(
+    sapply(tbl$x$tag$attribs$columns, `[[`, "name"),
+    c("PI", "POC", "Project name", "Network", "Project code",
+      "Detections", "Individuals", "PI_emails", "POC_emails" )
+  )
+})
+
+
+
+test_that("creates a js table for tag data", {
+  matched <- read.csv(matched_path)
+
+  tbl <- match_table(matched, "tag")
+
+  expect_s3_class(tbl, c('reactable', 'htmlwidget'), exact = TRUE)
+  expect_type(tbl$x, 'list')
+
+  # 9 columns
+  expect_length(tbl$x$tag$attribs$columns, 9)
+
+  # Correct names
+  expect_equal(
+    sapply(tbl$x$tag$attribs$columns, `[[`, 'name'),
+    c("PI", "POC", "Project name", "Network", "Project code",
+      "Detections", "Individuals", "PI_emails", "POC_emails" )
+  )
 })
