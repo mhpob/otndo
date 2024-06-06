6efcad3128ce27bc9b2abf42225e4dd89b71b584
Mike O'Brien <mikeob9@gmail.com>
2024-06-06 16:23:42 -0400

    update urls

--- R/make_receiver_push_summary.R
+++ R/make_receiver_push_summary.R
@@ -29,7 +29,8 @@
 #' download.file(
 #'   paste0(
 #'     "https://members.oceantrack.org/data/repository/pbsm/",
-#'     "data-and-metadata/2018/pbsm-instrument-deployment-short-form-2018.xls"
+#'     "data-and-metadata/2018/pbsm-instrument-deployment-short-form-2018.xls/",
+#'     "@@download/file"
 #'   ),
 #'   destfile = file.path(td, "pbsm-instrument-deployment-short-form-2018.xls"),
 #'   mode = "wb"
@@ -39,7 +40,7 @@
 #' download.file(
 #'   paste0(
 #'     "https://members.oceantrack.org/data/repository/pbsm/",
-#'     "detection-extracts/pbsm_qualified_detections_2018.zip",
+#'     "detection-extracts/pbsm_qualified_detections_2018.zip/",
 #'     "@@download/file"
 #'   ),
 #'   destfile = file.path(td, "pbsm_qualified_detections_2018.zip"),
@@ -53,7 +54,7 @@
 #' download.file(
 #'   paste0(
 #'     "https://members.oceantrack.org/data/repository/pbsm/",
-#'     "detection-extracts/pbsm_unqualified_detections_2018.zip",
+#'     "detection-extracts/pbsm_unqualified_detections_2018.zip/",
 #'     "@@download/file"
 #'   ),
 #'   destfile = file.path(td, "pbsm_unqualified_detections_2018.zip"),
