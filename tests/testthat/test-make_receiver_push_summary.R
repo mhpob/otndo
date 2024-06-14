8d07ff989c7a37d2fd6bda826645d4f21a079130
Mike O'Brien <mikeob9@gmail.com>
2024-06-14 12:57:15 -0400

    add test and note around rendering with RMarkdown

--- tests/testthat/test-make_receiver_push_summary.R
+++ tests/testthat/test-make_receiver_push_summary.R
@@ -14,3 +14,22 @@ test_that("Projects are summarized", {
 
   expect_true(any(grepl("receiver_push_summary", list.files(getwd()))))
 })
+
+
+
+
+test_that("Renders with RMarkdown", {
+  make_receiver_push_summary(
+    qualified = pbsm$qualified,
+    unqualified = pbsm$unqualified,
+    deployment = pbsm$deployment,
+    since = "2018-05-06",
+    rmd = TRUE
+  ) |>
+    expect_message("Asking OTN GeoServer for project information") |>
+    expect_message("Writing report") |>
+    expect_message("RMarkdown") |>
+    expect_message("Done")
+
+  expect_true(any(grepl("receiver_push_summary", list.files(getwd()))))
+})
--- tests/testthat/test-make_tag_push_summary.R
+++ tests/testthat/test-make_tag_push_summary.R
@@ -14,6 +14,26 @@ test_that("Non-ACT projects are summarized", {
   expect_true(any(grepl("tag_push_summary", list.files(getwd()))))
 })
 
+
+
+
+test_that("Renders with RMarkdown", {
+  make_tag_push_summary(
+    matched = pbsm$matched,
+    since = "2018-05-06",
+    rmd = TRUE
+  ) |>
+    expect_message("Asking OTN GeoServer") |>
+    expect_message("Writing report") |>
+    expect_message("RMarkdown") |>
+    expect_message("Done")
+
+  expect_true(any(grepl("tag_push_summary", list.files(getwd()))))
+})
+
+
+
+
 test_that("summarizes with no new detections", {
   skip("Bug still exists")
 })
