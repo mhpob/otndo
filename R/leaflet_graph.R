c3bb92157f96bdc71ec8540a5d801c6914ed2f20
JOY LIU <113057199+joyliujoyliu@users.noreply.github.com>
2024-06-27 13:51:21 -0300

    test 1

---
+++ R/leaflet_graph.R
@@ -0,0 +1,47 @@
+#' @param extract OTN station_spatial
+#' @export
+#'
+library(leaflet)
+library(leaflegend)
+library(tidyr)
+library(tidyverse)
+
+
+leaflet_graph <- function(station_spatial) {
+  df <- station_spatial %>% tidyr::extract(geometry, c('lon', 'lat'), '\\((.*), (.*)\\)', convert = TRUE)
+  df %>%  as.data.frame()
+  numPal <- colorNumeric('viridis', df $Detections)
+  leaflet::leaflet(data = df) %>%
+    addTiles() %>%
+    addCircleMarkers( lat = ~lat, lng = ~lon,
+                      color= ~numPal(Detections), fillColor = ~numPal(Detections),fillOpacity = 0.7, popup =  paste("Station ", df$station , "<br>",
+                                                                                                                    "PI:", df$PI, "<br>",
+                                                                                                                    "Detections:", df$Detections, "<br>",
+                                                                                                                    "detectedby:", df$detectedby, "<br>",
+                                                                                                                    "Individuals:", df$Individuals),
+                      radius = ~Individuals) %>%
+
+    leaflegend::addLegendSize(
+      values = df $Individuals,
+      baseSize = 1,
+      color = 'black',
+      title = 'Individual',
+      shape = 'circle',
+      orientation = 'horizontal',
+      opacity = .5,
+      fillOpacity = 0,
+      breaks = 5,
+      position = 'bottomright') %>%
+    leaflegend::addLegendNumeric(
+      pal = numPal,
+      title = 'Matched Detection',
+      shape = 'stadium',
+      values = df $Detections,
+      fillOpacity = .5,
+      decreasing = TRUE,
+      position = 'bottomright')
+
+
+
+}
+
