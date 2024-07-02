0c9113429e5c5731708339a04dbdfa147baf8b1c
JOY LIU <113057199+joyliujoyliu@users.noreply.github.com>
2024-07-02 14:58:23 -0300

    color scale fix

--- R/leaflet_graph.R
+++ R/leaflet_graph.R
@@ -5,17 +5,17 @@
 leaflet_graph <- function(station_spatial) {
   geometry<- NULL
   df <- station_spatial |> tidyr::extract(geometry, c('lon', 'lat'), '\\((.*), (.*)\\)', convert = TRUE)
-  numPal <- leaflet::colorNumeric('viridis', df $Detections, reverse = TRUE)
+  numPal <- leaflet::colorNumeric('viridis', df $Detections)
   leaflet::leaflet(data = df) |>
     leaflet::addTiles() |>
     leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
-                      color= 'black', fillColor = ~numPal(Detections),fillOpacity = 0.7,
+                      color= 'black', fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
                       popup =  paste("Station ", df$station , "<br>",
                                      "PI:", df$PI, "<br>",
                                     "Detections:", df$Detections, "<br>",
                                     "detectedby:", df$detectedby, "<br>",
                                     "Individuals:", df$Individuals),
-                      radius = ~(Individuals/2)) |>
+                      radius = ~(df$Individuals)) |>
 
     leaflegend::addLegendSize(
       values = df $Individuals,
@@ -32,7 +32,7 @@ leaflet_graph <- function(station_spatial) {
       pal = numPal,
       title = 'Matched Detection',
       shape = 'stadium',
-      values = df $Detections,
+      values = df$Detections,
       fillOpacity = .5,
       decreasing = TRUE,
       position = 'bottomright')
