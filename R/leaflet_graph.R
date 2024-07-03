2c111cfab67d062dc19204bbe997606cbf2e9804
JOY LIU <113057199+joyliujoyliu@users.noreply.github.com>
2024-07-03 16:25:49 -0300

    I did several testing on the size of the individual, it might be use 2 different ways to show the size.

--- R/leaflet_graph.R
+++ R/leaflet_graph.R
@@ -8,7 +8,10 @@ leaflet_graph <- function(station_spatial) {
   geometry <- NULL
   df <- station_spatial |> tidyr::extract(geometry, c("lon", "lat"), "\\((.*), (.*)\\)", convert = TRUE)
   numPal <- leaflet::colorNumeric("viridis", df$Detections)
-  leaflet::leaflet(data = df) |>
+  
+  
+  if(max(df$Individuals)<31)
+  {leaflet::leaflet(data = df) |>
     leaflet::addTiles() |>
     leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
                       color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
@@ -21,16 +24,16 @@ leaflet_graph <- function(station_spatial) {
 
     leaflegend::addLegendSize(
       values = df $Individuals,
-      baseSize =  max(quakes$mag)/1.5,
+      baseSize =  max(quakes$mag),
       color = 'black',
       title = 'Individual',
       shape = 'circle',
       orientation = 'horizontal',
       opacity = .5,
       fillOpacity = 0,
-      breaks = 10,
+      breaks = min((max(quakes$mag)-min((quakes$mag))),5),
       position = 'bottomright',
-      stacked = TRUE) |>
+      stacked = F) |>
     leaflegend::addLegendNumeric(
       pal = numPal,
       title = 'Matched Detections',
@@ -42,5 +45,44 @@ leaflet_graph <- function(station_spatial) {
       position = 'bottomright',
       height = 20,
       width = 100
-    )
+    )}
+  
+  else{leaflet::leaflet(data = df) |>
+      leaflet::addTiles() |>
+      leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
+                                 color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
+                                 popup =  paste("Station ", df$station , "<br>",
+                                                "PI:", df$PI, "<br>",
+                                                "Detections:", df$Detections, "<br>",
+                                                "detectedby:", df$detectedby, "<br>",
+                                                "Individuals:", df$Individuals),
+                                 radius = ~(df$Individuals)) |>
+      
+      leaflegend::addLegendSize(
+        values = df $Individuals,
+        baseSize =  max(quakes$mag),
+        color = 'black',
+        title = 'Individual',
+        shape = 'circle',
+        orientation = 'horizontal',
+        opacity = .5,
+        fillOpacity = 0,
+        breaks = min((max(quakes$mag)-min((quakes$mag))),7),
+        position = 'bottomright',
+        stacked = T) |>
+      leaflegend::addLegendNumeric(
+        pal = numPal,
+        title = 'Matched Detections',
+        values = df$Detections,
+        fillOpacity = .5,
+        decreasing = FALSE,
+        orientation = 'horizontal',
+        shape = 'rect',
+        position = 'bottomright',
+        height = 20,
+        width = 100
+      )
+    
+  }
+  
 }
