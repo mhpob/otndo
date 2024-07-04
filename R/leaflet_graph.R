663eb1d6da4ae3ecf602611d082e7df877a57496
JOY LIU <113057199+joyliujoyliu@users.noreply.github.com>
2024-07-04 13:54:17 -0300

    fixed color problem

--- R/leaflet_graph.R
+++ R/leaflet_graph.R
@@ -9,154 +9,7 @@ leaflet_graph <- function(station_spatial) {
   df <- station_spatial |> tidyr::extract(geometry, c("lon", "lat"), "\\((.*), (.*)\\)", convert = TRUE)
   numPal <- leaflet::colorNumeric("viridis", df$Detections)
 
-  #Test 1
-  if(max(df$Individuals)<31){
-    leaflet::leaflet(data = df) |>
-    leaflet::addTiles() |>
-    leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
-                      color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
-                      popup =  paste("Station ", df$station , "<br>",
-                                     "PI:", df$PI, "<br>",
-                                    "Detections:", df$Detections, "<br>",
-                                    "detectedby:", df$detectedby, "<br>",
-                                    "Individuals:", df$Individuals),
-                      radius = ~(df$Individuals)) |>
-    leaflegend::addLegendSize(
-      values = df $Individuals,
-      baseSize =  max(df$Individuals)/min((max(df$Individuals)-min((df$Individuals))),5),
-      color = 'black',
-      title = 'Individual',
-      shape = 'circle',
-      orientation = 'horizontal',
-      opacity = .5,
-      fillOpacity = 0,
-      breaks = min((max(df$Individuals)-min((df$Individuals))),5),
-      position = 'bottomright',
-      stacked = F) |>
-    leaflegend::addLegendNumeric(
-      pal = numPal,
-      title = 'Matched Detections',
-      values = df$Detections,
-      fillOpacity = .5,
-      decreasing = FALSE,
-      orientation = 'horizontal',
-      shape = 'rect',
-      position = 'bottomright',
-      height = 20,
-      width = 100
-    )}else{
-      leaflet::leaflet(data = df) |>
-      leaflet::addTiles() |>
-      leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
-                                 color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
-                                 popup =  paste("Station ", df$station , "<br>",
-                                                "PI:", df$PI, "<br>",
-                                                "Detections:", df$Detections, "<br>",
-                                                "detectedby:", df$detectedby, "<br>",
-                                                "Individuals:", df$Individuals),
-                                 radius = ~(df$Individuals)) |>
-      leaflegend::addLegendSize(
-        values = df $Individuals,
-        baseSize =  max(df$Individuals)/min((max(df$Individuals)-min((df$Individuals))),7),
-        color = 'black',
-        title = 'Individual',
-        shape = 'circle',
-        orientation = 'horizontal',
-        opacity = .5,
-        fillOpacity = 0,
-        breaks = min((max(df$Individuals)-min((df$Individuals))),7),
-        position = 'bottomright',
-        stacked = T) |>
-      leaflegend::addLegendNumeric(
-        pal = numPal,
-        title = 'Matched Detections',
-        values = df$Detections,
-        fillOpacity = .5,
-        decreasing = FALSE,
-        orientation = 'horizontal',
-        shape = 'rect',
-        position = 'bottomright',
-        height = 20,
-        width = 100
-      )
-    }
-
-
-
-  #Test 2
-
-  if(max(df$Individuals)<31){
-    leaflet::leaflet(data = df) |>
-      leaflet::addTiles() |>
-      leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
-                                 color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
-                                 popup =  paste("Station ", df$station , "<br>",
-                                                "PI:", df$PI, "<br>",
-                                                "Detections:", df$Detections, "<br>",
-                                                "detectedby:", df$detectedby, "<br>",
-                                                "Individuals:", df$Individuals),
-                                 radius = ~(df$Individuals)) |>
-      leaflegend::addLegendSize(
-        values = df $Individuals,
-        baseSize =  max(df$Individuals)/min((max(df$Individuals)-min((df$Individuals))),5),
-        color = 'black',
-        title = 'Individual',
-        shape = 'circle',
-        orientation = 'horizontal',
-        opacity = .5,
-        fillOpacity = 0,
-        breaks = min((max(df$Individuals)-min((df$Individuals))),5),
-        position = 'bottomright',
-        stacked = F) |>
-      leaflegend::addLegendNumeric(
-        pal = numPal,
-        title = 'Matched Detections',
-        values = df$Detections,
-        fillOpacity = .5,
-        decreasing = FALSE,
-        orientation = 'horizontal',
-        shape = 'rect',
-        position = 'bottomright',
-        height = 20,
-        width = 100
-      )}else{
-        leaflet::leaflet(data = df) |>
-          leaflet::addTiles() |>
-          leaflet::addCircleMarkers( lat = ~lat, lng = ~lon,
-                                     color= ~numPal(df$Detections), fillColor = ~numPal(df$Detections),fillOpacity = 0.7,
-                                     popup =  paste("Station ", df$station , "<br>",
-                                                    "PI:", df$PI, "<br>",
-                                                    "Detections:", df$Detections, "<br>",
-                                                    "detectedby:", df$detectedby, "<br>",
-                                                    "Individuals:", df$Individuals),
-                                     radius = ~(df$Individuals)) |>
-          leaflegend::addLegendSize(
-            values = df $Individuals,
-            baseSize =  max(df$Individuals)/min((max(df$Individuals)-min((df$Individuals))),7,mean(df$Individuals)),
-            color = 'black',
-            title = 'Individual',
-            shape = 'circle',
-            orientation = 'horizontal',
-            opacity = .5,
-            fillOpacity = 0,
-            breaks = min((max(df$Individuals)-min((df$Individuals))),7),
-            position = 'bottomright',
-            stacked = F) |>
-          leaflegend::addLegendNumeric(
-            pal = numPal,
-            title = 'Matched Detections',
-            values = df$Detections,
-            fillOpacity = .5,
-            decreasing = FALSE,
-            orientation = 'horizontal',
-            shape = 'rect',
-            position = 'bottomright',
-            height = 20,
-            width = 100
-          )
-      }
-
-
+  
 # test 3
 
 
@@ -166,16 +19,19 @@ leaflet_graph <- function(station_spatial) {
                    lat = ~lat,
                    lng = ~lon,
                    shape = 'circle',
-                   color =  ~numPal(df$Detections),
-                   fillColor = ~numPal(df$Detections),
+                   color =  ~numPal(Detections),
+                   fillColor = ~numPal(Detections),
                    opacity = .5,
+                   baseSize = 5,
                    popup =  paste("Station ", df$station , "<br>",
                                   "PI:", df$PI, "<br>",
                                   "Detections:", df$Detections, "<br>",
                                   "detectedby:", df$detectedby, "<br>",
                                   "Individuals:", df$Individuals)) |>
-    leaflegend::addLegendSize(
-      values = df $Individuals,
+    leaflegend::  addLegendSize(
+      values =  quakes$Individuals,
+      baseSize = 5,
+      color = 'black',
       title = 'Individual',
       shape = 'circle',
       orientation = 'horizontal',
@@ -183,7 +39,7 @@ leaflet_graph <- function(station_spatial) {
       fillOpacity = 0,
       breaks = min((max(df$Individuals)-min((df$Individuals))),7),
       position = 'bottomright',
-      stacked = F) |>
+      stacked = T)|>
     leaflegend::addLegendNumeric(
       pal = numPal,
       title = 'Matched Detections',
